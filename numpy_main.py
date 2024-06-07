e40dfddd6d9c491ac1d34743a7cafe40ad31d637
xiaoshunren <85426803+xiaoshunren@users.noreply.github.com>
2024-06-07 16:04:27 +0900

    Add files via upload

--- numpy_main.py
+++ numpy_main.py
@@ -158,3 +158,77 @@ print('------------------------')
 print(c[0][1]) # 拿到第1页第1行
 print('------------------------')
 print(c[0,1,0]) # 拿到数组中的4  位置在0页1行0列
+print('------------------------')
+for i in range(c.shape[0]):
+    for j in range(c.shape[1]):
+        for k in range(c.shape[2]):
+            print(c[i,j,k]) # i页j行k列
+print('------------------------')
+
+
+print('----------知識ポイント補足--------------')
+# argmin() 和 argmax() 两个函数分别对应着求矩阵中最小元素和最大元素的索引。
+A = np.arange(2,14).reshape((3,4)) 
+# array([[ 2, 3, 4, 5]
+#        [ 6, 7, 8, 9]
+#        [10,11,12,13]])
+print(np.argmin(A))    # 0
+print(np.argmax(A))    # 11
+print(np.mean(A))        # 均值
+print(np.median(A))       # 中位数
+print('---------------------')
+A = np.arange(12).reshape((3, 4))
+print(A)
+#纵向分割
+print(np.split(A, 2, axis=1))
+#横向分割
+print(np.split(A, 3, axis=0))
+#不等量分割
+print(np.array_split(A, 3, axis=1)) # # 沿轴1将数组分成3个子数组 axis: 指定要分割的轴。默认值为 0。
+
+print('---------数列合并------------')
+A = np.array([1,1,1])
+B = np.array([2,2,2])
+         
+print(np.vstack((A,B)))    # vertical stack本身属于一种上下合并，即对括号中的两个整体进行对应操作
+D = np.hstack((A,B))       # horizontal stack D本身来源于A，B两个数列的左右合并，而且新生成的D
+
+print(D) 
+# [1,1,1,2,2,2]
+print('---------------------')
+
+ # 视图变维度(数据共享) reshape() ・ravel()
+
+a = np.arange(1,9) 
+print(a) # 数列1-8
+print('---------------------')
+b=a.reshape(2,4) # 视图变维 →　2行4列二维数组
+print(b)
+print('---------------------')
+c=b.reshape(2,2,2) # 视图变维 →　2页2行2列三维数组
+print(c)
+print('---------------------')
+d=c.ravel() # 视图变维 →　一维数组
+print(d)
+print('---------------------')
+
+# 复制变维度(数据独立) flatten()
+e = c.flatten() # 把三维数组变成一维显示
+print(e)
+print('---------------------')
+
+# 数组切片操作
+# 步长+:默认切 头到尾
+# 步长-:默认切 尾到头
+# 默认步长 1
+# 数组对象[起始位置:终止位置:步长,......]
+
+a = np.arange(1,10)
+print(a) # 1 2 3 4 5 6 7 8
+print(a[:3]) # 9 8 7 6 5 4 3 2 1
+print(a[3:6])#  4 5 6 
+print(a[6:]) # 7 8 9
+print(a[::-1]) #  9 8 7 6 5 4 3 2 1
+print(a[:-4:-1])# 9 8 7
+print(a[-4:-7:-1])# 6 5 4
+print(a[-7::-1])# 3 2 1
