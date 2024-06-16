f7d0f7a44521750c7426c89ba7b6421171ba9141
xiaoshunren <85426803+xiaoshunren@users.noreply.github.com>
2024-06-16 14:56:24 +0900

    Add files via upload

---
+++ Ai-Study/KADAI/boston_house_price.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as mp # 数据可视化
+import pandas as pd # 序列高级函数
+import scipy as sc # 科学计算
+import numpy as np # 基础值算法
+
+#  影响因素
+#  CRIM（人均犯罪率） ; NOX（一氧化氮浓度） ; RM（住宅房间数）; AGE（1940年以前建造的住宅比例）; DIS（距离波士顿五个主要就业中心的加权距离）
+#  RAD（高速公路可达性指数） ; TAX（每万美元的财产税率） ; PTRATIO（师生比）; B（黑人比例） ; LSTAT（低收入人群比例）; MEDV（自住房屋的中位数价格）
+
